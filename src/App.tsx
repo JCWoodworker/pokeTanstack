f485f64042f9a0428784323c7cb5fc42beeaeba0
JCWoodworker <jc@rilocalwoodworks.com>
2025-03-04 01:35:11 -0500

    set up pokemon api, added tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+import { useEffect, useState } from "react"
+
+interface Pokemon {
+  name: string
+  url: string
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemon, setPokemon] = useState<Pokemon[]>([])
+  const getPokeData = async (limit: number, offset: number) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+    const data = await response.json()
+    setPokemon(data.results)
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  console.log(pokemon)  
+
+  useEffect(() => {
+    getPokeData(20, 20)
+  }, [])
+
+	return (
+		<>
+			<div>
+				<h1>Testing Tanstack Query</h1>
+        {pokemon.map((pokemon, index) => (
+          <div key={pokemon.name}>
+            <h2>{pokemon.name} - {index + 1}</h2>
+          </div>
+        ))}
+			</div>
+		</>
+	)
 }
 
 export default App
