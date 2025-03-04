be0064bfa365e754c2c7e7ca22b53835f8b801ff
JCWoodworker <jc@rilocalwoodworks.com>
2025-03-04 10:08:52 -0500

    updated styling of pagination again

--- src/pagination.tsx
+++ src/pagination.tsx
@@ -15,21 +15,28 @@ const Pagination: React.FC<PaginationProps> = ({
 
 	return (
 		<div className="pagination-container">
-			<button disabled={page === 1} onClick={() => setPage(page - 1)}>
-				Prev
-			</button>
-			{Array.from({ length: totalPages }, (_, index) => (
-				<button
-					className={page === index + 1 ? "activePaginationButton" : ""}
-					key={index}
-					onClick={() => setPage(index + 1)}
-				>
-					{index + 1}
+			<div className="pagination-buttons-container">
+				<button disabled={page === 1} onClick={() => setPage(page - 1)}>
+					Prev
 				</button>
-			))}
-			<button disabled={page === totalPages} onClick={() => setPage(page + 1)}>
-				Next
-			</button>
+				<button
+					disabled={page === totalPages}
+					onClick={() => setPage(page + 1)}
+				>
+					Next
+				</button>
+			</div>
+			<div className="pagination-numbers-container">
+				{Array.from({ length: totalPages }, (_, index) => (
+					<button
+						className={page === index + 1 ? "activePaginationButton" : ""}
+						key={index}
+						onClick={() => setPage(index + 1)}
+					>
+						{index + 1}
+					</button>
+				))}
+			</div>
 		</div>
 	)
 }
